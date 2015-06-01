270c92242f054f98067eff6eda2cc9c646fa1b5c
jnfaujfgjv <ujfvufvwuv>
2015-06-01 09:33:25 -0300

    Revert "Teste do funcionamento do GitHub"
    This reverts commit 00f78ae5dfa8bd0f42764ab689377271a39a8fc2.

--- Silicon_Quest/main.c
+++ Silicon_Quest/main.c
@@ -1,1 +1,290 @@
-Teste - Pull Request
+/*
+
+		Allegro 5 Shell Application v1.1
+			by Mike Geig
+
+		This code is provided for education purposes and comes with no warantee
+		either implicit or explicit.
+
+		This code is free to use however you see fit.
+*/
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+//==============================================
+//GLOBALS
+//==============================================
+
+bool keys[] = {false, false, false, false, false};
+enum KEYS {UP, DOWN, LEFT, RIGHT, SPACE};
+
+int colunas = 20;
+int linhas = 20;
+
+int matriz[20][20]=
+{
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+};
+
+int pos_matriz_x = 0;
+int pos_matriz_y = 0;
+
+bool done = false;
+bool draw = true;
+int pos_x = WIDTH / 2;
+int pos_y = HEIGHT / 2;
+int coluna;
+int linha;
+
+
+int main(void)
+{
+    //==============================================
+    //SHELL VARIABLES
+    //==============================================
+    bool done = false;
+    bool render = false;
+
+    float gameTime = 0;
+    int frames = 0;
+    int gameFPS = 0;
+
+    //==============================================
+    //PROJECT VARIABLES
+    //==============================================
+	int xOff = 0;
+	int yOff = 0;
+
+    //==============================================
+    //ALLEGRO VARIABLES
+    //==============================================
+    ALLEGRO_DISPLAY *display = NULL;
+    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+    ALLEGRO_TIMER *timer;
+    ALLEGRO_FONT *font18;
+
+    //==============================================
+    //ALLEGRO INIT FUNCTIONS
+    //==============================================
+    if(!al_init())										//initialize Allegro
+        return -1;
+
+    display = al_create_display(WIDTH, HEIGHT);			//create our display object
+
+    if(!display)										//test display object
+        return -1;
+
+    //==============================================
+    //ADDON INSTALL
+    //==============================================
+    al_install_keyboard();xOff -= keys[RIGHT] * 10;
+    al_install_mouse();
+    al_init_image_addon();
+    al_init_font_addon();
+    al_init_ttf_addon();
+    al_init_primitives_addon();
+
+    //==============================================
+    //PROJECT INIT
+    //==============================================
+    font18 = al_load_font("arial.ttf", 18, 0);
+
+    //==============================================
+    //TIMER INIT AND STARTUP
+    //==============================================
+    event_queue = al_create_event_queue();
+    timer = al_create_timer(1.0 / 60);
+
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_mouse_event_source());
+
+    al_start_timer(timer);
+    gameTime = al_current_time();
+    //al_hide_mouse_cursor(display);
+    while(!done)
+    {
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);
+
+        //==============================================
+        //INPUT
+        //==============================================
+        if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+        {
+            done = true;
+        }
+        if(ev.type == ALLEGRO_EVENT_MOUSE_AXES)
+        {
+            pos_x = ev.mouse.x;
+            pos_y = ev.mouse.y;
+        }
+        if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
+        {
+            if(ev.mouse.button & 1)
+            {
+                coluna = (pos_x - pos_matriz_x) / 80;
+                linha = (pos_y - pos_matriz_y) / 60;
+            }
+            if(matriz[linha][coluna] == 1)
+			{
+				matriz[linha][coluna] = 0;
+			}
+
+            if(ev.mouse.button & 2)
+            {
+                done = true;
+            }
+        }
+
+        if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
+        {
+            switch(ev.keyboard.keycode)
+            {
+            case ALLEGRO_KEY_ESCAPE:
+                done = true;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                keys[LEFT] = true;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                keys[RIGHT] = true;
+                break;
+            case ALLEGRO_KEY_UP:
+                keys[UP] = true;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                keys[DOWN] = true;
+                break;
+            case ALLEGRO_KEY_SPACE:
+                keys[SPACE] = true;
+                break;
+            }
+        }
+        if(ev.type == ALLEGRO_EVENT_KEY_UP)
+        {
+            switch(ev.keyboard.keycode)
+            {
+            case ALLEGRO_KEY_ESCAPE:
+                done = true;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                keys[LEFT] = false;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                keys[RIGHT] = false;
+                break;
+            case ALLEGRO_KEY_UP:
+                keys[UP] = false;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                keys[DOWN] = false;
+                break;
+            case ALLEGRO_KEY_SPACE:
+                keys[SPACE] = false;
+                break;
+            }
+
+        }
+
+        //==============================================
+        //GAME UPDATE
+        //==============================================
+        if(ev.type == ALLEGRO_EVENT_TIMER)
+        {
+            render = true;
+
+            //UPDATE FPS===========
+            frames++;
+            if(al_current_time() - gameTime >= 1)
+            {
+                gameTime = al_current_time();
+                gameFPS = frames;
+                frames = 0;
+            }
+            //=====================
+
+            if(!(xOff >= 0))
+				xOff += keys[LEFT] * 10;
+			if(!(yOff >= 0))
+				yOff += keys[UP] * 10;
+			if(!(xOff <= ((-20 * 80) + 800)))
+				xOff -= keys[RIGHT] * 10;
+			if(!(yOff <= ((-20 * 60) + 600)))
+				yOff -= keys[DOWN] * 10;
+        }
+
+        //==============================================
+        //RENDER
+        //==============================================
+        if(render && al_is_event_queue_empty(event_queue))
+        {
+            render = false;
+
+
+            //BEGIN PROJECT RENDER================
+            int j, i;
+            for(i=0; i<20; i++)
+            {
+                for(j=0; j<20; j++)
+                    switch(matriz[i][j])
+                    {
+                    case 0:
+                        al_draw_filled_rectangle(xOff+j*80, yOff+i*60, xOff+(j*80)+80,yOff+(i*60)+60, al_map_rgb(122, 0, 0));
+                        break;
+                    case 1:
+                        al_draw_filled_rectangle(xOff+j*80, yOff+i*60, xOff+(j*80)+80,yOff+(i*60)+60, al_map_rgb(122, 0, 122));
+                        break;
+                    }
+            }
+            if(draw)
+            {
+                al_draw_rectangle(pos_x, pos_y, pos_x + 80, pos_y + 60, al_map_rgb(0, 0, 0), 1);
+            }
+            al_draw_textf(font18, al_map_rgb(255, 0, 255), 5, 5, 0, "FPS: %i", gameFPS);	//display FPS on screen
+
+            //FLIP BUFFERS========================
+            al_flip_display();
+            al_clear_to_color(al_map_rgb(0,0,0));
+        }
+    }
+
+    //==============================================
+    //DESTROY PROJECT OBJECTS
+    //==============================================
+
+    //SHELL OBJECTS=================================
+    al_destroy_font(font18);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(event_queue);
+    al_destroy_display(display);
+
+    return 0;
+}
